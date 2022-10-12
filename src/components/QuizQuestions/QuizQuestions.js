f75e1a0d1ec3b48f01001c4e95d03b2e5d1eb395
ehsan-info <ehsanurrahman1992@gmail.com>
2022-10-12 06:03:11 +0300

    question answer toast added

--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { EyeIcon } from '@heroicons/react/24/solid';
+import React, { useState } from 'react';
+import { EyeIcon, StopCircleIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const QuizQuestions = ({ questions }) => {
     const { id, correctAnswer, question, options } = questions;
     const processedQuestion = question.replace(/&nbsp;/gi, ' ').slice(3, -4);
+    const setClicked = (questionText) => {
+        if (questionText === correctAnswer) {
+            toast.success('Correct Answer !', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+        else {
+            toast.error('Wrong Answer !', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+    }
     return (
         <div className='question-lists'>
             <div className='my-4 border-2 rounded-md border-orange-500 bg-slate-300 p-4'>
@@ -12,11 +26,12 @@ const QuizQuestions = ({ questions }) => {
                     <Link><EyeIcon className="h-6 w-6" /></Link>
                 </div>
                 <div className=''>
-                    <div className='grid grid-cols-2 gap-4'>
-                        <button>{options[0]}</button>
-                        <button>{options[1]}</button>
-                        <button>{options[2]}</button>
-                        <button>{options[3]}</button>
+                    <div className='grid grid-cols-2 gap-4 items-center'>
+                        <button onClick={() => setClicked(`${options[0]}`)} className='flex items-center p-4 border-2 rounded-md w-2/4 hover:bg-orange-200'><StopCircleIcon className="h-4 w-4 mr-2" />{options[0]}</button>
+                        <button onClick={() => setClicked(`${options[1]}`)} className='flex items-center p-4 border-2 rounded-md w-2/4 hover:bg-orange-200'><StopCircleIcon className="h-4 w-4 mr-2" />{options[1]}</button>
+                        <button onClick={() => setClicked(`${options[2]}`)} className='flex items-center p-4 border-2 rounded-md w-2/4 hover:bg-orange-200'><StopCircleIcon className="h-4 w-4 mr-2" />{options[2]}</button>
+                        <button onClick={() => setClicked(`${options[3]}`)} className='flex items-center p-4 border-2 rounded-md w-2/4 hover:bg-orange-200'><StopCircleIcon className="h-4 w-4 mr-2" />{options[3]}</button>
+                        <ToastContainer />
                     </div>
                 </div>
 
